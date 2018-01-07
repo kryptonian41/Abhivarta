ee7d9373b76efed46b99c2f7c0342294708c8125
Apoorv Agarwal <aapoorv41@gmail.com>
2018-01-07 22:41:37 +0530

    Major changes

--- scripts/external/parallax.js
+++ scripts/external/parallax.js
@@ -1,14 +1,27 @@
 $(window).scroll(function() {
   var wscroll = $(this).scrollTop();
-  console.log(wscroll);
+  // console.log(wscroll);
   $(".header__hero-image").css({
     transform: "translate(0," + wscroll / 3 + "px)"
   });
   $(".parallax-container").css({
     transform: "translate("+ wscroll/2 +"px,-" + wscroll / 3 + "px)"
   });
-  if (wscroll > $('.section-about').offset().top) {
-    console.log("flag");
-    $('.dome-image').addClass('animate animate-backward animate-slidein-bottom');
+  // if (wscroll > $('.section-about--abhivarta').offset().top - 200) {
+  //   // console.log("flag-abhivarta");
+  //   $('.section-about--abhivarta').children('.slidein-image').addClass('animate animate-backward animate-slidein-bottom');
+  // }
+  // if (wscroll > $('.section-about--manipal').offset().top) {
+  //   // console.log("flag-manipal");
+  //   $('.section-about--manipal').children('.slidein-image').addClass('animate animate-backward animate-slidein-bottom');
+  // }
+  var css1 = {
+    transform : "translateX(-"+ ($('.about-card__manipal--left').offset().top - wscroll) +"px)"
   }
+  var css2 = {
+    transform :"translateX("+($('.about-card__abhivarta--right').offset().top - wscroll) +"px)"
+  }
+  $('.about-card__manipal--left').children('.backdrop').css(css1);
+  $('.about-card__abhivarta--right').children('.backdrop').css(css2);
+  console.log($('.about-card__manipal--left').children('.backdrop'));
 });
--- scripts/main.js
+++ scripts/main.js
@@ -16,4 +16,33 @@ $(document).ready(function() {
     }
   });
   // end for navigation tab
+
+
+});
+
+
+var type1 = new Typed('.typed1',{
+    strings: ["About Abhivarta"],
+    // Optionally use an HTML element to grab strings from (must wrap each string in a <p>)
+    stringsElement: null,
+    // typing speed
+    typeSpeed: 30,
+    // time before typing starts
+    startDelay: 1200,
+    // backspacing speed
+    backSpeed: 20,
+    // time before backspacing
+    backDelay: 500,
+    // loop
+    loop: false,
+    // false = infinite
+    loopCount: false,
+    // show cursor
+    showCursor: false,
+    // character for cursor
+    cursorChar: "|",
+    // attribute to type (null == text)
+    attr: null,
+    // either html or text
+    contentType: 'html'
 });
